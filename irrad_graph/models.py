98cd56409edea8423861f9b06f10c58c9f0b50c7
Chris Kollias <chriskollias500@gmail.com>
2020-02-28 18:56:03 -0500

    graph image now appearing after submission

--- irrad_graph/models.py
+++ irrad_graph/models.py
@@ -6,4 +6,4 @@ class IrradGraphInputs(models.Model):
     latitude = models.DecimalField(max_digits=7, decimal_places=4)
     longitude = models.DecimalField(max_digits=7, decimal_places=4)
     year = models.IntegerField()
-    image = models.ImageField(null=True, upload_to='irrad_graph')
+    image = models.ImageField(default='irrad_graph/empty.png', upload_to='irrad_graph')
--- irrad_graph/views.py
+++ irrad_graph/views.py
@@ -5,19 +5,18 @@ import matplotlib.pyplot as plt
 
 from API.solar_api import NREL_API
 
-MEDIA_PATH = 'media/irrad_graph/'
 
 # Create your views here.
 def main_view(request, *args, **kwargs):
+    APP_MEDIA_PATH = 'irrad_graph/'
+    APP_SAVE_LOCATION = 'media/irrad_graph/'
 
     form = LatLongForm()
+    obj = None
 
     if request.method == 'POST':
         form = LatLongForm(request.POST)
-        print('CHECKPOINT A')
         if form.is_valid():
-            print('CHECKPOINT B')
-            IrradGraphInputs.objects.create(**form.cleaned_data)
             latitude = form.cleaned_data['latitude']
             longitude = form.cleaned_data['longitude']
             year = form.cleaned_data['year']
@@ -27,16 +26,18 @@ def main_view(request, *args, **kwargs):
 
             api = NREL_API()
             graph_image = api.get_monthly_averages(latitude, longitude, year, attrs)
-            image_path = MEDIA_PATH + graph_image
-            plt.savefig(image_path, dpi=300)
-            print('CHECKPOINT C')
+            image_path = APP_MEDIA_PATH + graph_image
+            APP_SAVE_LOCATION += graph_image
+            plt.savefig(APP_SAVE_LOCATION, dpi=300)
+            form.cleaned_data['image'] = image_path
+            obj = IrradGraphInputs.objects.create(**form.cleaned_data)
 
             form = LatLongForm()
         else:
             print(form.errors)
 
     context = {
-        'hello': 'Hello Solar!',
         'form': form,
+        'obj': obj,
     }
     return render(request, 'irrad_graph/irrad_graph_page.html', context)
